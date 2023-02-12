ae33320e7063b0c263937aa712d899b9cea9a61a
SIBTAIN-ASAD <sibtainasad50@gmail.com>
2023-02-12 20:47:56 +0500

    3rd commit, Added dependencies for excel file

--- app/src/main/java/com/example/quran_navigation/MainActivity.java
+++ app/src/main/java/com/example/quran_navigation/MainActivity.java
@@ -1,8 +1,17 @@
 package com.example.quran_navigation;
 
-import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +19,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        readExcelFileFromAssets();
+
+    }
+
+
+    public void readExcelFileFromAssets() {
+
+        try {
+            InputStream inputStream = getAssets().open("data.xlsx");
+            XSSFWorkbook workbook = new XSSFWorkbook(inputStream);
+            XSSFSheet sheet = workbook.getSheetAt(0);
+            for (Row row : sheet) {
+                for (Cell cell : row) {
+                    System.out.print(cell.toString() + " ");
+                }
+                System.out.println();
+            }
+            workbook.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 }
