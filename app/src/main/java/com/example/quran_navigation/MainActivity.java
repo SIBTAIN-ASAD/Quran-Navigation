caa1a42b5943a3bd19d59d0a0500d0d396a3fc8b
SIBTAIN-ASAD <sibtainasad50@gmail.com>
2023-02-13 04:26:21 +0500

    4th commit, data has been reading

--- app/src/main/java/com/example/quran_navigation/MainActivity.java
+++ app/src/main/java/com/example/quran_navigation/MainActivity.java
@@ -2,44 +2,61 @@ package com.example.quran_navigation;
 
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import org.apache.poi.ss.usermodel.Cell;
-import org.apache.poi.ss.usermodel.Row;
-import org.apache.poi.xssf.usermodel.XSSFSheet;
-import org.apache.poi.xssf.usermodel.XSSFWorkbook;
-
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
-import java.io.InputStream;
+
+
 
 public class MainActivity extends AppCompatActivity {
 
+    public TextView txt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        readExcelFileFromAssets();
+        System.out.println("ABCD");
+            readExcelFileFromAssets();
+        txt = findViewById(R.id.textView);
 
     }
 
 
     public void readExcelFileFromAssets() {
-
+        BufferedReader reader = null;
         try {
-            InputStream inputStream = getAssets().open("data.xlsx");
-            XSSFWorkbook workbook = new XSSFWorkbook(inputStream);
-            XSSFSheet sheet = workbook.getSheetAt(0);
-            for (Row row : sheet) {
-                for (Cell cell : row) {
-                    System.out.print(cell.toString() + " ");
-                }
-                System.out.println();
+            reader = new BufferedReader(new FileReader("QuranMetaData.xlsx"));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        String line = null;
+        while (true) {
+            try {
+                line = reader.readLine();
+                if (line == null) break;
+            } catch (IOException e) {
+                e.printStackTrace();
             }
-            workbook.close();
+            finally {
+
+            }
+            String[] values = line.split(",");
+            for (String value : values) {
+                String Value = value;
+                txt.setText(Value);
+                System.out.print(value + "\t");
+            }
+            System.out.println();
+        }
+        try {
+            reader.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
-
     }
 }
